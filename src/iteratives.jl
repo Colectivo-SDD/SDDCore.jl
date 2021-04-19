fb497893ad43a61e7595a801fee7cfc4c1ff010b
Renato Leriche Vázquez <r_lerichev@ciencias.unam.mx>
2021-04-19 12:32:28 -0500

    Macro @iterative.

--- src/iteratives.jl
+++ src/iteratives.jl
@@ -2,7 +2,7 @@
 """
     iterative(T,f,n) -> Function
 
-Create the \$n\$-iterative of a function \$f\$.
+Create the \$n\$-iterative of an one-valuated function \$f\$.
 """
 @inline function iterative(::Type{T}, f::Function, n::Integer) where {T <: Number}
     function fn(t::T)
@@ -50,3 +50,35 @@ Create the \$n\$-iterative of a function \$f:\\mathbb{R}^2\\rightarrow\\mathbb{R
         xn, yn
     end # function
 end # function
+
+
+"""
+    @iterative(f,n)
+
+    Create the \$n\$-iterative of an one-valuated function \$f\$.
+"""
+macro iterative(f,n)
+    expr_fx = :(f(x))
+    for i in 2:eval(n)
+        expr_copy = copy(expr_fx)
+        expr_fx.args[2] = expr_copy
+    end
+    return :(x->$expr_fx)
+end
+
+"""
+    @iterativeR2(f,n)
+
+    Create the \$n\$-iterative of a function \$f:\\mathbb{R}^2\\rightarrow\\mathbb{R}^2\$.
+"""
+macro iterativeR2(f,n)
+    expr_fxy = :(f(x,y))
+    if eval(n) > 1
+        expr_fxy = :(f(f(x,y)...))
+        for i in 3:eval(n)
+            expr_copy = copy(expr_fxy)
+            expr_fxy.args[2] = expr_copy
+        end
+    end
+    return :((x,y)->$expr_fxy)
+end
